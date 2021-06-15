44cd558d77a3b38760b758548aae987dfe9d6838
dawwik <dawid.wiktorowski22@gmail.com>
2021-06-15 23:12:14 +0200

    added connect with database

---
+++ src/main/java/com/dawidwiktorowski/Pizzeria/config/DataSourceProvider.java
@@ -0,0 +1,20 @@
+package com.dawidwiktorowski.Pizzeria.config;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DataSourceProvider {
+    private static DataSource dataSource;
+    private DataSourceProvider(){}
+
+    public static DataSource getDaraSource() throws NamingException{
+        if(dataSource == null){
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env/");
+            dataSource = (DataSource) envContext.lookup("jdbc/pizzeria");
+        }
+        return dataSource;
+    }
+}
